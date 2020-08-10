bec409cd1fcaa50a94c5dec2cdd037ab9cfc3505
Bruno <brunormferreira@hotmail.com>
2020-08-10 15:57:39 -0300

    add(feat): interfaces

---
+++ interfaces.ts
@@ -0,0 +1,34 @@
+interface Game {
+  title: string;
+  description: string;
+  readonly genre: string;
+  platform?: string[];
+  getSimilars?: (title: string) => void
+}
+
+const tlou: Game = {
+  title: 'The last of US',
+  description: 'The best game in the world',
+  genre: 'Action',
+  getSimilars: (title: string) => {
+    console.log(`Similar games to ${title}: WoW, Mario, FcK`);
+  }
+}
+
+if (tlou.getSimilars) {
+  tlou.getSimilars(tlou.title)
+}
+
+interface DLC extends Game {
+  originalGame: Game;
+  newContent: string[];
+}
+
+const leftBehind: DLC = {
+  title: 'The last of US - Left Behind',
+  description: '...',
+  genre: 'Action',
+  platform: ['PS4'],
+  originalGame: tlou,
+  newContent: ['3 Hours of Expansion', 'new characters']
+}
